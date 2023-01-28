ba29823fef3a99dc3004f6e6ea92963ccb294243
sandmark <sandmark.m@gmail.com>
2023-01-28 09:52:22 +0900

    Qualify package name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-template.mount-app "0.1.0-SNAPSHOT"
+(defproject sandmark/lein-template.mount-app "0.1.1"
   :description "A Leiningen template for reloadable project using mount"
   :url "https://github.com/sandmark/mount-app"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
